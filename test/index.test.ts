2be2b1639d026ab4766947f4ecd34b9169026c4a
Chris <chrisbreuer93@gmail.com>
2025-03-12 10:31:55 -0700

    chore: initial commit
    chore: wip
    
    chore: wip
    
    chore: wip
    
    chore: wip
    
    chore: wip
    
    chore: wip
    
    chore: wip
    
    chore: wip
    
    chore: wip
    
    chore: wip
    
    chore: wip
    
    chore: wip
    
    chore: wip
    
    chore: wip
    
    chore: wip
    
    chore: wip
    
    chore: wip
    
    chore: wip
    
    chore: wip

---
+++ test/index.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from 'bun:test'
+// import { afterAll, afterEach, beforeAll, beforeEach, describe, expect, it, mock, spyOn } from 'bun:test'
+
+describe('vite-plugin-dotenvx', () => {
+  it('should work', () => {
+    expect(true).toBe(true)
+  })
+})
